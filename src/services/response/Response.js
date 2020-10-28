625e160c7b4cab2daa663185ab992259155ac50e
Erick Saravia <ericks@labs.aeegle.com>
2020-10-27 21:32:20 -0600

    fix: (testController) resolver static scope in class

--- src/services/response/Response.js
+++ src/services/response/Response.js
@@ -1,10 +1,10 @@
 class Response {
-  ping (res) {
+  static ping (res) {
     let response = {code: 1, status: 200, name: 'Ok', message: 'Server Online'}
     res.status(200).json(response)
   }
 
-  successList (res, data) {
+  static successList (res, data) {
     let response = {
       total_items: data.totalDocs,
       current_page: data.page,
@@ -14,39 +14,37 @@ class Response {
     res.status(200).json(response)
   }
 
-  success (res, data) {
+  static success (res, data) {
     res.status(200).json(data)
   }
 
-  successCreate (res, data) {
+  static successCreate (res, data) {
     res.status(201).json(data)
   }
 
-  notFound (res, message) {
+  static notFound (res, message) {
     const response = {code: 0, status: 404, name: 'Not Found', message}
     res.status(404).json(response)
   }
 
-  badRequest (res, message = 'bad request') {
+  static badRequest (res, message = 'bad request') {
     const response = {code: 0, status: 400, name: 'Bad Request', message}
     res.status(400).json(response)
   }
 
-  unauthorized (res, message = 'Your request was made with invalid credentials') {
+  static unauthorized (res, message = 'Your request was made with invalid credentials') {
     const response = {code: 0, status: 401, name: 'Unauthorized', message}
     res.status(401).json(response)
   }
 
-  internalServerError (res, message = 'was an internal server error occurred') {
+  static internalServerError (res, message = 'was an internal server error occurred') {
     const response = {code: 0, status: 500, name: 'Internal Server Error', message}
     res.status(500).json(response)
   }
 
-  dataValidationFailed (res, response) {
+  static dataValidationFailed (res, response) {
     res.status(422).json(response)
   }
 }
-
-
 
 export default Response
