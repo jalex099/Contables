e6fe04b6e4640e74bcfdb050fce59b6186d33440
Javier Morales <mm18014@gmail.com>
2020-11-01 15:14:28 -0600

    Creacion del proyecto Front-End

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Hola</h1>
+  
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+  font-weight:bold
+}
+</style>
