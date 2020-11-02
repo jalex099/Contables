7362004007a306cc8778c1ae398c588b49e6499a
Javier Morales <mm18014@gmail.com>
2020-11-02 13:46:10 -0600

    Creacion de menu y espacio de trabajo

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,17 +1,19 @@
 <template>
+  <header>
             <!-- Always shows a header, even in smaller screens. -->
         <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
         <header class="mdl-layout__header">
             <div class="mdl-layout__header-row">
             <!-- Title -->
-            <span class="mdl-layout-title">Sistemas Contables</span>
+            <span class="mdl-layout-title">Title</span>
             <!-- Add spacer, to align navigation to the right -->
             <div class="mdl-layout-spacer"></div>
             <!-- Navigation. We hide it in small screens. -->
             <nav class="mdl-navigation mdl-layout--large-screen-only">
-                <a class="mdl-navigation__link" href="">Inicio</a>
-                <a class="mdl-navigation__link" href="">Asientos Contables</a>
-                <a class="mdl-navigation__link" href="">Nuevo asiento</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
+                <a class="mdl-navigation__link" href="">Link</a>
             </nav>
             </div>
         </header>
@@ -25,32 +27,22 @@
             </nav>
         </div>
         <main class="mdl-layout__content">
-            <div class="page-content"><!-- Your content goes here -->
-  <HelloWorld msg="Welcome to Your Vue.js App"/></div>
+            <div class="page-content"><!-- Your content goes here --></div>
         </main>
         </div>
+  </header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'NavBar'
 }
+
+
 </script>
 
-<style>
-#app {
-  font-family: 'Quicksand', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-html,body{
-  margin: 0;
-  padding: 0;
-}
-
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h4{background-color: blue;}
 </style>
