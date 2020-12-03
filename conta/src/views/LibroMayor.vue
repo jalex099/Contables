43544df4b5cc730357750fdd78346defce06fb98
Javier Morales <mm18014@gmail.com>
2020-12-03 14:04:21 -0600

    Finalizacion del proyecto

--- conta/src/views/LibroMayor.vue
+++ conta/src/views/LibroMayor.vue
@@ -1,7 +1,69 @@
 <template>
     <b-row class="w-100">
       <b-col cols="12"> 
-        {{cuentas}}
+        <h3 class="display-4 px-2 py-4" style="font-family: 'Quicksand', sans-serif;">Libro Mayor</h3>
+        <hr>
+        <div v-for="element in cuentas" :key="element" class="container px-5 py-2">
+          <div v-if="element['name']=='Activos'">
+            <h3>Activos</h3>
+            <div v-for="exe in element['sub_accounts']" :key="exe">
+              <div class="flex-box" v-if="exe['current_amount']>0">
+                <p class="text-muted">{{exe['name']}}</p>
+                <p class="text-muted">$ {{exe['current_amount']}}</p>
+              </div>
+            </div>
+          </div>
+
+          <div v-if="element['name']=='Pasivos'">
+            <h3>Pasivos</h3>
+            <div v-for="exe in element['sub_accounts']" :key="exe">
+              <div class="flex-box" v-if="exe['current_amount']>0">
+                <p class="text-muted">{{exe['name']}}</p>
+                <p class="text-muted">$ {{exe['current_amount']}}</p>
+              </div>
+            </div>
+          </div>
+
+          <div v-if="element['name']=='Capital'">
+            <h3>Capital</h3>
+            <div v-for="exe in element['sub_accounts']" :key="exe">
+              <div class="flex-box" v-if="exe['current_amount']>0">
+                <p class="text-muted">{{exe['name']}}</p>
+                <p class="text-muted">4 {{exe['current_amount']}}</p>
+              </div>
+            </div>
+          </div>
+
+          <div v-if="element['name']=='Ingresos'">
+            <h3>Ingresos</h3>
+            <div v-for="exe in element['sub_accounts']" :key="exe">
+              <div class="flex-box" v-if="exe['current_amount']>0">
+                <p class="text-muted">{{exe['name']}}</p>
+                <p class="text-muted">$ {{exe['current_amount']}}</p>
+              </div>
+            </div>
+          </div>
+
+          <div v-if="element['name']=='Costos'">
+            <h3>Costos</h3>
+            <div v-for="exe in element['sub_accounts']" :key="exe">
+              <div class="flex-box" v-if="exe['current_amount']>0">
+                <p class="text-muted">{{exe['name']}}</p>
+                <p class="text-muted">$ {{exe['current_amount']}}</p>
+              </div>
+            </div>
+          </div>
+
+          <div v-if="element['name']=='Gastos'">
+            <h3>Gastos</h3>
+            <div v-for="exe in element['sub_accounts']" :key="exe">
+              <div class="flex-box" v-if="exe['current_amount']>0">
+                <p class="text-muted">{{exe['name']}}</p>
+                <p class="text-muted">$ {{exe['current_amount']}}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </b-col>
     </b-row>
 </template>
@@ -42,3 +104,11 @@ export default {
   }
 }
 </script>
+
+<style  scoped>
+.flex-box{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+</style>
